5695568130941b23cee3c310e2b5081aed73af0f
Peter Pham <ptrpham2468@gmail.com>
2022-11-18 11:13:03 -0600

    added list files funct in controller. Added constants in protocol file

--- src/controller.py
+++ src/controller.py
@@ -1,16 +1,81 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import socket
+import threading
+import protocol
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    serverSocket = establishConnection()
+    choice = menu()
+    if choice == 1:
+        sendListRequest(serverSocket)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#############################################################################################################
+# Function:            establishConnection
+# Author:              Peter Pham (pxp180041)
+# Date Started:        08/10/2022
+#
+# Description:
+# Attempts to connect to the server
+#############################################################################################################
+def establishConnection():
+    try:
+        # AF_INET for IPv4          SOCK_DGRAM for UDP
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serverSocket.connect((protocol.SERVER_IP, protocol.SERVER_PORT))
+        return serverSocket
+
+    except socket.error:
+        print("Unable to connect to server")
+
+
+#############################################################################################################
+# Function:            Menu
+# Author:              Peter Pham (pxp180041)
+# Date Started:        08/10/2022
+#
+# Description:
+# Display a menu where the user can decide what to do
+#############################################################################################################
+def menu():
+    while True:
+        print("1. Get file names")
+        print("2. Request File")
+        print("3. Exit")
+        try:
+            num = int(input("Enter a number: "))
+            if num > 0 & num < 4:
+                return num
+        except ValueError:
+            print("Not a number try again.")
+
+
+#############################################################################################################
+# Function:            sendRequest
+# Author:              Peter Pham (pxp180041)
+# Date Started:        08/10/2022
+#
+# Description:
+# Looks at the user choice and send the appropriate message to the server
+#
+# @param    choice      int         contains what the user wants to do
+# @param    serverSocket    socket  contains the socket needed to send the request
+#############################################################################################################
+def sendListRequest(serverSocket):
+
+    serverSocket.sendall(protocol.LIST_REQUEST)
+    serverSocket.listen()
+    connectSocket, addr = serverSocket.accept()
+
+    msg = connectSocket.recv(1024)
+
+    # TODO: Print files here after knowing the contents sent by the server
+    print("List of files")
+    for file in msg.split().decode():
+        print(file)
+
+
+
+
+if __name__ == "__main__":
+    main()
